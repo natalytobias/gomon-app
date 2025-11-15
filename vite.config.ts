d846b622f4e2903464f4c357e3520866293109a2
Ana Júlia Souza <anajuliapereiradesouza003@gmail.com>
2025-11-14 21:52:12 -0400

    Adicionado talwind e lucide icon para ajustes de ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    // React + React Compiler
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
+
+    // Tailwind v4 como plugin do Vite
+    tailwindcss(),
   ],
-})
+});
